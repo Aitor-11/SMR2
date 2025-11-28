cd38a3d978281d9e875204183888f9412fd29b8b
Aitor-11 <aitormotahhirgonzalez5241@gmail.com>
2025-11-28 19:37:37 +0100

    Update examen_aitor1.py

--- Python/examen_aitor1.py
+++ Python/examen_aitor1.py
@@ -20,27 +20,7 @@ for n in numeros:
 
 print(f"Los números son: {numeros}")
 print(f"Sus factoriales son: {factoriales}")
-# filepath: /Users/aitormotahhir/Python/examen_aitor1.py
-# factorial de varios números
-num = int(input("dime un numero : "))
-num2 = int(input("dime otro numero : "))
-num3 = int(input("dime otro numero mas: "))
 
-def factorial(n):
-    f = 1
-    for i in range(1, n + 1):
-        f *= i
-    return f
-
-numeros = [num, num2, num3]
-factoriales = []
-
-for n in numeros:
-    fact = factorial(n)
-    factoriales.append(fact)
-    print(f"El factorial de {n} es {fact}")
-
-print(f"Los números son: {numeros}")
 
 '''
 1. Solicitar al usuario que ingrese un número y almacenarlo en 'num'
