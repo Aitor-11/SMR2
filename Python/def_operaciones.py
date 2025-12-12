c38c86c3468d0c8935173ad5911fa28b74160fd4
Aitor-11 <aitormotahhirgonzalez5241@gmail.com>
2025-12-12 18:34:09 +0100

    Update def_operaciones.py

--- Python/def_operaciones.py
+++ Python/def_operaciones.py
@@ -1,12 +1,26 @@
 a = int(input("dame un numero : "))
 b = int(input("dame otro numero: "))
 
+def suma(a, b):
+    return a + b
+resultado = suma(a, b)
+
+print("la suma de los nuemros es ", resultado)
+
+def resta(a, b):
+    return a - b
+
+print("la resta de los nuemros es ", resta(a, b))
+
+def multiplicacion(a, b):
+    return a * b
+
+print("la multiplicacion de los nuemros es ", multiplicacion(a, b))
 
 
 def division(a, b):
-    if a or b == 0 :
+    if a == 0 :
             return "ERROR 0 NO ES DIVISIBLE"
-resultado = a / b
-
+    resultado = a / b
 
 print("la division de los nuemros es ", division(a, b))
